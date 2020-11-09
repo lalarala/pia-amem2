ea9a527679ee27d461a00bc3fe8d41d39f9730b7
Lucas Nery <lucasnerytoledo@hotmail.com>
2020-11-09 15:03:57 -0300

    Parte do post feita

--- js/script.js
+++ js/script.js
@@ -1,21 +1,55 @@
+const botao = document.querySelector('.botao')
+const mensagem = document.getElementById('mensagem')
+const urlPost = 'https://news-api-node.herokuapp.com/api/v1/news/936cce4d-110c-40f2-b1c1-26a49cfe3484'
+const postFeitos = document.querySelector('.post-feitos')
+var novoElemento = document.createElement('p')
+
+function mostrarMensagens() {
+
+    fetch(urlPost)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+
+        if(postFeitos) {
+            for(var i = 0; i < data.length; i++) {
+                var mensagemNova = document.createTextNode([i + 1] + " - " + data[i].post)
+                var novoElemento = document.createElement('p')
+
+                novoElemento.appendChild(mensagemNova)
+                postFeitos.appendChild(novoElemento)
+            }
+        }
+
+    });
+
+}
+mostrarMensagens()
+
+
 function enviarMensagem() {
-    const botao = document.querySelector('.botao')
-    const mensagem = document.getElementById('mensagem')
-    const urlPost = 'https://news-api-node.herokuapp.com/api/v1/news/936cce4d-110c-40f2-b1c1-26a49cfe3484'
 
     if(botao) {
         botao.addEventListener('click', function() {
-            fetch(urlPost)
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(data) {
-                for(var i = 0; i < data.length; i++) {
-                    console.log(data[i].post);
-                }
-            });
+            var poster = {
+                post: mensagem.value
+            };
+        
+            fetch('https://news-api-node.herokuapp.com/api/v1/news/936cce4d-110c-40f2-b1c1-26a49cfe3484',
+                {
+                    method: 'post',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(poster)
+                })
+                .then(results => results.json())
+                mostrarMensagens()
         })
     }
+
 }
 enviarMensagem()
 
@@ -24,16 +58,21 @@ const animacaoDeScrollar = () => {
 	const sessentaPorCentoDaTela = window.innerHeight * 0.6
 
 	function animarAoScrollar() {
+
 		section.forEach((itens) => {
 			const distanciaAteOTopo = itens.getBoundingClientRect().top
-			const metadeDaTela = distanciaAteOTopo - sessentaPorCentoDaTela
+            const metadeDaTela = distanciaAteOTopo - sessentaPorCentoDaTela
+            
 			if(metadeDaTela < 0) {
 				itens.classList.add('animar')
-			}
-		})
+            }
+            
+        })
+        
 	}
 
 	animarAoScrollar()
-	window.addEventListener('scroll', animarAoScrollar)
+    window.addEventListener('scroll', animarAoScrollar)
+    
 }
 animacaoDeScrollar()
